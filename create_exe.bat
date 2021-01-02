095e5b3797b71efc115f15f30f5aac7083e34ddf
jscin <joshscina@gmail.com>
2021-01-02 10:23:06 -0500

    Fixed script

--- create_exe.bat
+++ create_exe.bat
@@ -1,3 +1,7 @@
 @echo off
 pyinstaller -D -w -i=locked.ico PyPass.py
 copy %CD%\locked.ico %CD%\dist\PyPass\locked.ico
+rd /s /q %CD%\build
+move /y %CD%\dist\PyPass %CD%\PyPass
+rd /s /q %CD%\dist
+del %CD%\PyPass.spec

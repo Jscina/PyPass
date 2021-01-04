6f2feea4f811f8f9edb4ffbd5c366312326250e1
jscin <joshscina@gmail.com>
2021-01-03 19:57:07 -0500

    Added comment

--- create_exe.bat
+++ create_exe.bat
@@ -1,4 +1,5 @@
 @echo off
+pip install cryptography PyQt5
 pyinstaller -F -w -i=locked.ico PyPass.py
 mkdir %CD%\PyPass
 copy %CD%\locked.ico %CD%\PyPass\locked.ico
@@ -9,3 +10,4 @@ rd /s /q %CD%\dist
 del %CD%\PyPass.spec
 7z a -SFX PyPass_SFX.exe PyPass
 rd /s /q %CD%\PyPass
+:: This requires python and 7-Zip to be installed and added to PATH

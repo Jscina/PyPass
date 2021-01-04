c33162e44d0859c33ce602ae9f67548a01110812
jscin <joshscina@gmail.com>
2021-01-03 19:53:59 -0500

    Modified the batch file to create sfx archive

--- create_exe.bat
+++ create_exe.bat
@@ -1,7 +1,11 @@
 @echo off
-pyinstaller -D -w -i=locked.ico PyPass.py
-copy %CD%\locked.ico %CD%\dist\PyPass\locked.ico
+pyinstaller -F -w -i=locked.ico PyPass.py
+mkdir %CD%\PyPass
+copy %CD%\locked.ico %CD%\PyPass\locked.ico
+copy %CD%\LICENSE %CD%\PyPass\LICENSE
 rd /s /q %CD%\build
-move /y %CD%\dist\PyPass %CD%\PyPass
+move /y %CD%\dist\PyPass.exe %CD%\PyPass\PyPass.exe
 rd /s /q %CD%\dist
 del %CD%\PyPass.spec
+7z a -SFX PyPass_SFX.exe PyPass
+rd /s /q %CD%\PyPass
